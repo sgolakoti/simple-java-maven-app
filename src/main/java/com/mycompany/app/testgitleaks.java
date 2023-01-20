e71710598531eaf4be24dd1bc9fbb2ac12bc3e0d
sgolakoti <87377343+sgolakoti@users.noreply.github.com>
2023-01-20 10:29:24 +0530

    Create testgitleaks.java

---
+++ src/main/java/com/mycompany/app/testgitleaks.java
@@ -0,0 +1,16 @@
+import java.sql.*;
+public class Example {
+    private static final String PASSWORD = "mysecretpassword";
+    public static void main(String[] args) {
+        try {
+            // Connect to the database
+            Connection conn = DriverManager.getConnection("jdbc:mysql://hostname:port/database", "username", PASSWORD);
+            // Do something with the connection
+            // ...
+            // Close the connection
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
+        }
+    }
+}
